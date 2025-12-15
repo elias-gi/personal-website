b7ef38e05ee2e516e7e15b5e429b9fbb3d0440c1
elias-gi <ilias.giannakos@outlook.com>
2025-12-15 20:56:35 +0200

    added react bits

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -4,17 +4,22 @@ import { About } from './components/About';
 import { Experience } from './components/Experience';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
+import TextCursor from './components/TextCursor';
 
 function App() {
   return (
-    <Layout>
-      <Hero />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
-    </Layout>
+    <TextCursor text='📈'>
+      <Layout>
+        <Hero />
+        <About />
+        <Experience />
+        <Projects />
+        <Contact />
+      </Layout>
+    </TextCursor>
   );
 }
 
 export default App;
+
+/**#image="/logos/test.png"*/
