88db5c16f1e737418b82de46c24e8e73d3114e1a
elias-gi <ilias.giannakos@outlook.com>
2025-12-16 15:19:51 +0200

    fix

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -2,7 +2,6 @@ import { Layout } from './components/Layout';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Experience } from './components/Experience';
-import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 import TextCursor from './components/TextCursor';
 import LogoLoop from './components/LogoLoop';
